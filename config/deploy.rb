170914a2757854b9709c17d1affdfd4e85a3bec0
Adam Gyulavari <adam.gyulavari@lab.coop>
2018-04-11 14:41:33 +0200

    add loc email

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :rbenv_roles, :all # default value
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/konnekt-live.json"
+append :linked_files, "config/konnektlive-firebase.json"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
