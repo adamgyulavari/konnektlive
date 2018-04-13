88b20700dc8fc22f71f9c398c26c3854accd3148
Adam Gyulavari <adam.gyulavari@lab.coop>
2018-04-13 13:53:09 +0200

    change fixed texts to db

--- konnekt_live.rb
+++ konnekt_live.rb
@@ -6,7 +6,12 @@ class KonnektLive < Sinatra::Base
   private_key_json_string = File.open('config/konnektlive-firebase.json').read
   firebase = Firebase::Client.new("https://konnektlive.firebaseio.com", private_key_json_string)
 
+  before do
+    @texts = firebase.get('texts').body
+  end
+
   get '/' do
+    puts @texts.inspect
     haml :index
   end
 
